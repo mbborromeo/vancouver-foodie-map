27f2c40d64ab82d3c29cdeade7bef29bfa0b17da
mbborromeo <mbborromeo@gmail.com>
2022-11-04 14:22:21 -0700

    update mapPoints.js

--- src/data/mapPoints.js
+++ src/data/mapPoints.js
@@ -1,326 +1,386 @@
 export default {
   CafesAndBakeries: [
     {
-      label: 'Small Victory Bakery',
-      lat: 49.25859334438221, 
+      label: "Small Victory Bakery",
+      lat: 49.25859334438221,
       lng: -123.13873378462311,
-      desc: 'Modern, chic setting, with good coffee and excellent food!'
+      desc: "Modern, chic setting, with good coffee and excellent food!",
     },
     {
-      label: 'Whitewood Coffee',
-      lat: 49.257352522625744, 
+      label: "Whitewood Coffee",
+      lat: 49.257352522625744,
       lng: -123.12725827916513,
-      desc: 'Cozy place that serves a hearty scrambled egg on toast!'
+      desc: "Cozy place that serves a hearty scrambled egg on toast!",
     },
     {
-      label: 'Velo Star Cafe & Bike Repair',
-      lat: 49.25716443111373, 
+      label: "Velo Star Cafe & Bike Repair",
+      lat: 49.25716443111373,
       lng: -123.12025200377371,
-      desc: 'Vintage corner store with decent coffee and surprisingly good food!'
+      desc: "Vintage corner store with decent coffee and surprisingly good food!",
     },
     {
-      label: 'Blenz Coffee',
-      lat: 49.26349295339335, 
-      lng: -123.11971221608198,
-      desc: "Everything's good at this local Canadian franchise"
-    },
-    {
-      label: 'Dalina',
-      lat: 49.263303806008096, 
+      label: "Dalina",
+      lat: 49.263303806008096,
       lng: -123.12641664794796,
-      desc: "Excellent all 'round!"
+      desc: "Excellent all 'round!",
     },
     {
-      label: 'Bean Around the World',
-      lat: 49.26229920410998, 
+      label: "Bean Around the World",
+      lat: 49.26229920410998,
       lng: -123.12239976920426,
-      desc: 'Unique drinks such as beet ginger latte'
+      desc: "Unique drinks such as beet ginger latte",
     },
     {
-      label: 'Beaucoup Bakery',
-      lat: 49.266577777753184, 
+      label: "Beaucoup Bakery",
+      lat: 49.266577777753184,
       lng: -123.14072383936174,
-      desc: "Must try their hot cocoa. It's really rich!"
+      desc: "Must try their hot cocoa. It's really rich!",
     },
     {
-      label: 'Blue Hat Bakery',
-      lat: 49.269950803986944, 
+      label: "Blue Hat Bakery",
+      lat: 49.269950803986944,
       lng: -123.1375628476882,
-      desc: 'Chefs in training serve up really good pastry, coffee and a wicked hot cocoa!'
+      desc: "Chefs in training serve up really good pastry, coffee and a wicked hot cocoa!",
     },
     {
-      label: 'Pallet Coffee Roasters',
-      lat: 49.26359654012127, 
+      label: "Tru Cafe",
+      lat: 49.269636925336606,
+      lng: -123.13873282945521,
+      desc: "Modern cafe with nice pastry, good customer service, and a rooftop patio. Open til late.",
+    },
+    {
+      label: "Pallet Coffee Roasters",
+      lat: 49.26359654012127,
       lng: -123.15079629443129,
-      desc: 'Damn good coffee!'
+      desc: "Damn good coffee!",
     },
     {
-      label: 'Platform 7 Coffee',
-      lat: 49.263775532222375, 
+      label: "Platform 7 Coffee",
+      lat: 49.263775532222375,
       lng: -123.15784109770138,
-      desc: 'Cool setting, great lunch sandwiches!'
+      desc: "Cool setting, great lunch sandwiches!",
     },
     {
-      label: 'Thomas Haas Chocolatier',
-      lat: 49.26423878286061, 
+      label: "Thomas Haas Chocolatier",
+      lat: 49.26423878286061,
       lng: -123.16335139396156,
-      desc: 'Awesome hot cocoa!'
+      desc: "The best hot chocolate!",
+    },
+    {
+      label: "La La Island",
+      lat: 49.26441383559297,
+      lng: -123.1714631141136,
+      desc: "Island themed cafe with interesting pastries and open til late.",
     },
     {
       label: "Juliet's Cafe",
-      lat: 49.272637404600964, 
+      lat: 49.272637404600964,
       lng: -123.14803722438617,
-      desc: 'Really good grilled chicken sandwich'
+      desc: "Really good grilled chicken sandwich",
     },
     {
-      label: 'Cafe Zen',
-      lat: 49.27117000256656, 
+      label: "Cafe Zen",
+      lat: 49.27117000256656,
       lng: -123.1552107186119,
-      desc: 'Lots of variety on the menu, and an excellent French toast!'
+      desc: "Lots of variety on the menu, and an excellent French toast!",
     },
     {
-      label: 'Viva Fine Foods & Bakery',
-      lat: 49.27209581182346, 
+      label: "Lumine Coffee",
+      lat: 49.26836690417294,
+      lng: -123.14967991855232,
+      desc: "Modern, cozy cafe with interesting croissants!",
+    },
+    {
+      label: "Viva Fine Foods & Bakery",
+      lat: 49.27209581182346,
       lng: -123.15513496061445,
-      desc: 'Excellent matcha latte and premium quality sandwiches, pastry and cookies!'
+      desc: "Excellent matcha latte and premium quality sandwiches, pastry and cookies!",
     },
     {
-      label: 'Blue Parrot',
-      lat: 49.27267772878422, 
+      label: "Blue Parrot",
+      lat: 49.27267772878422,
       lng: -123.13540418428299,
-      desc: 'A nice relaxing place with decent food and coffee, and an awesome view of Burrard Bridge. Try their foccacia sandwiches!'
+      desc: "A nice relaxing place with decent food and coffee, and an awesome view of Burrard Bridge. Try their foccacia sandwiches!",
     },
     {
-      label: 'Our Town Cafe',
-      lat: 49.26301729616128, 
-      lng: -123.09992144343515,
-      desc: 'Interesting corner setting with good coffee and food!'
+      label: "Off the Tracks",
+      lat: 49.27051640320023,
+      lng: -123.13405501092907,
+      desc: "Nestled away from the bustle of Granville Island, this 2-storey cafe serves up gourmet sandwiches and decent brew.",
     },
     {
-      label: 'Thierry Chocolatier Mt Pleasant',
-      lat: 49.26211593765953, 
+      label: "Forecast Coffee",
+      lat: 49.25917380574676,
+      lng: -123.10100221534321,
+      desc: "New cafe with plenty of food options and seating!",
+    },
+    {
+      label: "Thierry Chocolatier Mt Pleasant",
+      lat: 49.26211593765953,
       lng: -123.0998495944646,
-      desc: 'Excellent sandwich and soup combo'
+      desc: "Excellent sandwich and soup combo",
     },
     {
-      label: 'Milano Coffee Roasters',
-      lat: 49.263843350098405, 
+      label: "Milano Coffee Roasters",
+      lat: 49.263843350098405,
       lng: -123.10870006392572,
-      desc: 'Lots of coffee varieties with a nice vista of the park and city skyline'
+      desc: "Lots of coffee varieties with a nice vista of the park and city skyline",
     },
     {
-      label: 'Terra Breads',
-      lat: 49.27147526097092, 
+      label: "Terra Breads",
+      lat: 49.27147526097092,
       lng: -123.10681443907596,
-      desc: 'Excellent food, pastry and coffee!'
+      desc: "Excellent food, pastry and coffee!",
     },
     {
-      label: 'Caffe Artigiano - Yaletown',
-      lat: 49.274798669343184, 
+      label: "Faubourg",
+      lat: 49.274798669343184,
       lng: -123.12241934125637,
-      desc: 'Nice ambience and coffee'
+      desc: "Modern French cafe with interesting croissants",
     },
     {
-      label: 'Caffe Artigiano - Broadway',
-      lat: 49.26329200372624, 
-      lng: -123.10707922092897
+      label: "Caffe Artigiano - Broadway",
+      lat: 49.26329200372624,
+      lng: -123.10707922092897,
     },
     {
-      label: 'Coo Coo Coffee',
-      lat: 49.27581978214419, 
+      label: "Coo Coo Coffee",
+      lat: 49.27581978214419,
       lng: -123.12371161444106,
-      desc: 'Interesting drinks: try their organic ginger chai latte!'
+      desc: "Interesting drinks: try their organic ginger chai latte!",
     },
     {
-      label: 'Koko Monk Hot Chocolate Lounge',
-      lat: 49.264368103138786, 
+      label: "Koko Monk Hot Chocolate Lounge",
+      lat: 49.264368103138786,
       lng: -123.17001615971574,
-      desc: 'Unique hot chocolate concoctions drawing on inspiration from around the world!'
+      desc: "Unique hot chocolate concoctions drawing on inspiration from around the world!",
     },
     {
-      label: 'JJ Bean Coffee Roasters',
-      lat: 49.258401940945866, 
+      label: "JJ Bean Coffee Roasters",
+      lat: 49.258401940945866,
       lng: -123.1009314442571,
-      desc: 'Coffee always excellent, even their decaf!'
+      desc: "Coffee always excellent, even their decaf!",
     },
     {
-      label: 'Trees Coffee - Waterfront',
-      lat: 49.28494697791228, 
+      label: "Trees Coffee - Waterfront",
+      lat: 49.28494697791228,
       lng: -123.1142944238165,
-      desc: 'Live music on Friday nights'
+      desc: "Live music on Friday nights",
     },
     {
-      label: "Delany's Coffee House",
-      lat: 49.28880225601423, 
-      lng: -123.14067179719922,
-      desc: 'Good brew and food options'
+      label: "Railtown Cafe",
+      lat: 49.2853861340496,
+      lng: -123.11436731307563,
+      desc: "Gourmet sandwiches, soup, salad, coffee & tea",
     },
     {
-      label: 'Nemesis Coffee',
-      lat: 49.267165496929685, 
-      lng: -123.09262535975738,
-      desc: 'Damn good pastry!'
+      label: "Giovane cafe",
+      lat: 49.280857827337556,
+      lng: -123.11572001002088,
+      desc: "Modern cafe with good coffee, sandwiches and dessert",
     },
     {
       label: "Kafka's Coffee",
-      lat: 49.26731951686064, 
+      lat: 49.279023938994214,
+      lng: -123.11912843339849,
+      desc: "Modern little cafe next to Rainbow Park",
+    },
+    {
+      label: "Incognito Cafe",
+      lat: 49.28041438394251,
+      lng: -123.11970564096465,
+      desc: "Modern but cozy cafe nestled next to the VSO, serving up gourmet European style sandwiches",
+    },
+    {
+      label: "Perfecto cafe",
+      lat: 49.27690656933467,
+      lng: -123.12525833063418,
+      desc: "Modern Italian cafe with lots of pastry and sandwich options as well as gelato",
+    },
+    {
+      label: "Delany's Coffee House",
+      lat: 49.28880225601423,
+      lng: -123.14067179719922,
+      desc: "Good brew and food options",
+    },
+    {
+      label: "Nemesis Coffee",
+      lat: 49.267165496929685,
+      lng: -123.09262535975738,
+      desc: "Damn good pastry!",
+    },
+    {
+      label: "Kafka's Coffee",
+      lat: 49.26731951686064,
       lng: -123.09043667723262,
-      desc: 'Cozy little nook with a split-level setting'
+      desc: "Cozy little nook with a split-level setting",
     },
     {
-      label: 'Prado Cafe',
-      lat: 49.26948726431933, 
+      label: "Foglifter Coffee Roasters",
+      lat: 49.25321557168555,
+      lng: -123.10074541525522,
+      desc: "Modern cafe with gourmet sandwiches and a nice patio that catches the midday sun!",
+    },
+    {
+      label: "Trafiq Cafe & Bakery",
+      lat: 49.24739328067131,
+      lng: -123.10098330063724,
+      desc: "Cozy European cafe with rich pastries and gourmet sandwiches",
+    },
+    {
+      label: "Prado Cafe",
+      lat: 49.26948726431933,
       lng: -123.14096826463485,
-      desc: 'Decent coffee and sandwiches'
+      desc: "Decent coffee and sandwiches",
     },
     {
-      label: 'Bel Cafe',
-      lat: 49.26889460146715, 
+      label: "Bel Cafe",
+      lat: 49.26889460146715,
       lng: -123.14511524160119,
-      desc: 'Nice buzzing cafe with interesting baguettes!'
+      desc: "Nice buzzing cafe with interesting baguettes!",
     },
     {
-      label: 'Elysian Coffee',
-      lat: 49.26710490942158, 
+      label: "Elysian Coffee",
+      lat: 49.26710490942158,
       lng: -123.1451392861762,
-      desc: 'Decent coffee'
+      desc: "Decent coffee",
     },
     {
-      label: 'Enroute Bike Shop/Cafe',
-      lat: 49.270940220925645, 
+      label: "Enroute Bike Shop/Cafe",
+      lat: 49.270940220925645,
       lng: -123.15540973986501,
-      desc: 'Little off-the-grid cafe with nice coffee and pastry'
+      desc: "Little off-the-grid cafe with nice coffee and pastry",
     },
     {
-      label: 'Breka Bakery & Cafe',
-      lat: 49.26865677880061, 
+      label: "Breka Bakery & Cafe",
+      lat: 49.26865677880061,
       lng: -123.18680232801866,
-      desc: 'Good coffee, pastry and dessert, open 24/7'
+      desc: "Good coffee, pastry and dessert, open 24/7",
     },
     {
-      label: 'Grounds for Coffee',
-      lat: 49.26400888306555, 
+      label: "Grounds for Coffee",
+      lat: 49.26400888306555,
       lng: -123.1861604529745,
-      desc: 'Known for their cinnamon buns'
+      desc: "Known for their cinnamon buns",
     },
     {
-      label: 'The Birds and the Beets',
-      lat: 49.28365791771528, 
+      label: "The Birds and the Beets",
+      lat: 49.28365791771528,
       lng: -123.1028489228671,
-      desc: 'Cool place in the heart of Gastown'
+      desc: "Cool place in the heart of Gastown",
     },
     {
-      label: 'Cafe Villagio',
-      lat: 49.29127980949542, 
+      label: "Cafe Villagio",
+      lat: 49.29127980949542,
       lng: -123.12787067961791,
-      desc: 'Nice place to sit for a while near Coal Harbour'
+      desc: "Nice place to sit for a while near Coal Harbour",
     },
     {
-      label: 'Beyond Coffee',
-      lat: 49.280840035645525, 
+      label: "Beyond Coffee",
+      lat: 49.280840035645525,
       lng: -123.12702363473483,
-      desc: 'Makes an awesome breakfast English egg muffin'
+      desc: "Makes an awesome breakfast English egg muffin",
     },
     {
-      label: 'Modus Coffee',
-      lat: 49.262966314468116, 
+      label: "Modus Coffee",
+      lat: 49.262966314468116,
       lng: -123.10725579393409,
-      desc: 'Cozy, little cafe with good coffee and food'
+      desc: "Cozy, little cafe with good coffee and food",
     },
     {
-      label: 'Paragon Tea Room',
-      lat: 49.265707433214104, 
-      lng: -123.115086846949,
-      desc: 'Unique matcha misto oat latte and a life-sized model of Wall-e!'
+      label: "Paragon Tea Room",
+      lat: 49.256112437988826,
+      lng: -123.11544804602943,
+      desc: "Unique matcha misto oat latte and a life-sized model of Wall-e!",
     },
     {
-      label: 'Cafe Cittadella',
-      lat: 49.26487201356924, 
+      label: "Cafe Cittadella",
+      lat: 49.26487201356924,
       lng: -123.11726952339187,
-      desc: 'Interesting split-level home-style cafe with many a food and drink!'
+      desc: "Interesting split-level home-style cafe with many a food and drink!",
     },
     {
-      label: 'Matchstick Fraser St',
-      lat: 49.25731861663339, 
+      label: "Matchstick Fraser St",
+      lat: 49.25731861663339,
       lng: -123.09026880521802,
-      desc: 'Fresh bread and pastry, and interesting seasonal drinks such as cardamom ginger chai latte'
+      desc: "Fresh bread and pastry, and interesting seasonal drinks such as cardamom ginger chai latte",
     },
     {
-      label: 'Bean Around the World',
-      lat: 49.25953124064112, 
-      lng: -123.13883118974562
+      label: "Bean Around the World",
+      lat: 49.25953124064112,
+      lng: -123.13883118974562,
     },
     {
-      label: 'Trees Organic Coffee',
-      lat: 49.26361006696036, 
-      lng: -123.14077946178995
+      label: "Trees Organic Coffee",
+      lat: 49.26361006696036,
+      lng: -123.14077946178995,
     },
     {
-      label: 'Willow Cafe Bakery',
-      lat: 49.25719394900175, 
-      lng: -123.12190728174963
+      label: "Willow Cafe Bakery",
+      lat: 49.25719394900175,
+      lng: -123.12190728174963,
     },
     {
-      label: 'Mon Pitou Bakery',
-      lat: 49.26560354014844, 
+      label: "Mon Pitou Bakery",
+      lat: 49.26560354014844,
       lng: -123.1362842832091,
-      desc: 'Chic, little corner French cafe'
+      desc: "Chic, little corner French cafe",
     },
     {
-      label: 'Halfday Tea & Flower',
-      lat: 49.263526903254515, 
-      lng: -123.12399052182133
+      label: "Halfday Tea & Flower",
+      lat: 49.263526903254515,
+      lng: -123.12399052182133,
     },
     {
-      label: 'Elysian Coffee',
-      lat: 49.263136209031295, 
-      lng: -123.11729314750058
+      label: "Elysian Coffee",
+      lat: 49.263136209031295,
+      lng: -123.11729314750058,
     },
     {
-      label: 'Aperture Coffee Bar',
-      lat: 49.26333017773583, 
-      lng: -123.11048120087727
+      label: "Aperture Coffee Bar",
+      lat: 49.26333017773583,
+      lng: -123.11048120087727,
     },
     {
-      label: 'Black Coffeehouse',
-      lat: 49.26316437340903, 
-      lng: -123.10470895919948
-    }
+      label: "Black Coffeehouse",
+      lat: 49.26316437340903,
+      lng: -123.10470895919948,
+    },
   ],
 
   JuiceBars: [
     {
-      label: 'Heirloom Juice Company',
-      lat: 49.26023473372165, 
+      label: "Heirloom Juice Company",
+      lat: 49.26023473372165,
       lng: -123.13890175291127,
     },
   ],
 
   Restaurants: [
     {
-      label: 'Cafe Salade de Fruits',
-      lat: 49.265883970898535, 
+      label: "Cafe Salade de Fruits",
+      lat: 49.265883970898535,
       lng: -123.14023339089486,
     },
   ],
 
   Brunch: [
     {
-      label: 'Breakfast Table',
-      lat: 49.2586136391187, 
+      label: "Breakfast Table",
+      lat: 49.2586136391187,
       lng: -123.13836640566382,
     },
     {
       label: "Paul's Omelettery",
-      lat: 49.26605356028349, 
+      lat: 49.26605356028349,
       lng: -123.13870705522214,
     },
   ],
 
   Vegetarian: [
     {
-      label: 'Lotus Seed Vegan',
-      lat: 49.263204656918624, 
+      label: "Lotus Seed Vegan",
+      lat: 49.263204656918624,
       lng: -123.12105219497737,
     },
   ],
@@ -328,7 +388,7 @@ export default {
   Burgers: [
     {
       label: "Triple O's",
-      lat: 49.273433857625655, 
+      lat: 49.273433857625655,
       lng: -123.14717797537452,
     },
   ],
@@ -336,15 +396,15 @@ export default {
   PubsBars: [
     {
       label: "Carlos O'Bryan's Neighbourhood Pub",
-      lat: 49.26537928857413, 
+      lat: 49.26537928857413,
       lng: -123.1453758096267,
     },
   ],
 
   Parks: [
     {
-      label: 'Douglas Park',
-      lat: 49.2525429003053, 
+      label: "Douglas Park",
+      lat: 49.2525429003053,
       lng: -123.12330565030788,
     },
   ],
